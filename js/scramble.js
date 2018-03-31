118bf67cd08a5b25dea4f6e0b5a48f6d56324353
Trevor Jex <jextrevor@gmail.com>
2018-03-31 09:34:19 -0600

    Use requestAnimationFrame and compress image

--- js/scramble.js
+++ js/scramble.js
@@ -1,14 +1,17 @@
-words = ["TREVOR JEX","DESIGNER","THINKER","PROGRAMMER"]
+words = ["TREVOR JEX","DESIGNER","LEARNER","THINKER","CODER"]
 chars = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+//chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 index = 0;
+statictime = 5*60;
+dynamictime = 30;
+time = statictime;
+type = 0;
 function scramble(){
     index++;
     if(index >= words.length){
         index = 0;
     }
-    j = 15;
-    hello = setInterval(function(){step(7);j--;if(j<=0){clearInterval(hello);document.getElementById("scramble").innerHTML = words[index]}},50);
-    //document.getElementById("scramble").innerHTML = words[index];
+    document.getElementById("scramble").innerHTML = words[index];
 }
 function step(length){
     string = "";
@@ -17,5 +20,27 @@ function step(length){
     }
     document.getElementById("scramble").innerHTML = string;
 }
-scramble();
-setInterval(function(){scramble();},5000);
+function repaint(){
+    time--;
+    if(type != 0){
+        if(time > 0){
+            step(7);
+        }
+        else{
+            scramble();
+            type = 0;
+            time = statictime;
+        }
+    }
+    else{
+        if(time > 0){
+
+        }
+        else{
+            type = 1;
+            time = dynamictime;
+        }
+    }
+    requestAnimationFrame(repaint);
+}
+requestAnimationFrame(repaint);
