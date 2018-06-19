839f3c2ec86d593b38d7baa37a336e8b561365c2
Trevor Jex <jextrevor@gmail.com>
2018-06-19 16:28:39 -0600

    Consolidate a few CSS variables, switch to local fonts

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,5 @@ $('button[href^="#"]').click(function () {
     $('html, body').animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
     }, 500);
-
     return false;
 });
