4c37023213cc6d99ffe444bc5ea2f075c6865228
Trevor Jex <jextrevor@gmail.com>
2018-06-18 22:58:01 -0600

    Fix dialog fading in/out. Need to adjust fade time also for project info

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,18 @@
 function openDialog(){
     document.body.classList.add("modal");
     document.getElementById("dialog-overlay").classList.add("visible");
-    document.getElementById("dialog-overlay").classList.add("fadein");
     document.getElementById("dialog-container").classList.add("visible");
-    document.getElementById("dialog-container").classList.add("fadein");
+    setTimeout(function(){
+        document.getElementById("dialog-overlay").classList.add("fadein");
+        document.getElementById("dialog-container").classList.add("fadein");
+    },10);
 }
 function closeDialog(){
     document.body.classList.remove("modal");
-    document.getElementById("dialog-overlay").classList.remove("visible");
     document.getElementById("dialog-overlay").classList.remove("fadein");
-    document.getElementById("dialog-container").classList.remove("visible");
-    document.getElementById("dialog-overlay").classList.remove("fadein");
+    document.getElementById("dialog-container").classList.remove("fadein");
+    setTimeout(function(){
+        document.getElementById("dialog-overlay").classList.remove("visible");
+        document.getElementById("dialog-container").classList.remove("visible");
+    },500);
 }
