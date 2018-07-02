fac99ee8e72a395475041a6b545f2bc0dafe982b
Trevor Jex <jextrevor@gmail.com>
2018-07-02 14:49:52 -0600

    Lots of work

--- js/expand.js
+++ js/expand.js
@@ -1,6 +1,6 @@
 function expand(id,contid){
-    comp = document.getElementById(id);
-    cont = document.getElementById(contid);
+    var comp = document.getElementById(id);
+    var cont = document.getElementById(contid);
     var coords = comp.getBoundingClientRect();
     cont.style.top = coords.top + "px";
     cont.style.left = coords.left + "px";
@@ -15,14 +15,18 @@ function expand(id,contid){
     cont.style.width = "100%";
     cont.style.height = "100%";
     },10);
+    setTimeout(function(){
+        cont.style.overflow = "auto";
+    },500);
 }
 function hide(id,contid){
-    comp = document.getElementById(id);
-    cont = document.getElementById(contid);
+    var comp = document.getElementById(id);
+    var cont = document.getElementById(contid);
     var coords = comp.getBoundingClientRect();
     setTimeout(function(){
     comp.style.opacity = 1;
     cont.style.opacity = 0;
+    cont.style.overflow = "hidden";
     cont.style.top = coords.top + "px";
     cont.style.left = coords.left + "px";
     cont.style.width = (coords.right - coords.left) + "px";
