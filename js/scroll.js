f6d1567b0015076f7845990ae28fc34f8edeb0dd
Trevor Jex <jextrevor@gmail.com>
2018-11-10 11:58:18 -0700

    Removed old site stuff

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-//insert scrolling code here
-$('button[href^="#"]').click(function () {
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 500);
-    return false;
-});
